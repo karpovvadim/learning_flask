1171be513c34311c5b2c1f6d382ca439912d816a
karpovvadim <karpovvadim@mail.ru>
2023-02-16 23:18:37 +0300

    next_5

--- User’s_Guide/Tutorial/flaskr/__init__.py
+++ User’s_Guide/Tutorial/flaskr/__init__.py
@@ -4,29 +4,37 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the app
+    """Create and configure an instance of the Flask application
+    (Создайте и настройте экземпляр приложения Flask)."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # a default secret that should be overridden by instance config
+        # (секрет по умолчанию, который должен быть переопределен конфигурацией экземпляра)
+        SECRET_KEY="dev",
+        # store the database in the instance folder (сохранить базу данных в папке экземпляра)
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        # (загрузить конфигурацию экземпляра, если она существует, когда не тестируется)
+        app.config.from_pyfile("config.py", silent=True)
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# load the test config if passed in (загрузить тестовую конфигурацию, если она была передана)
+        app.config.update(test_config)
 
-    # ensure the instance folder exists
+    # ensure the instance folder exists (убедитесь, что папка экземпляра существует)
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
+
+    # register the database commands (зарегистрировать команды базы данных)
+    from flaskr import db
+    db.init_app(app)
 
     return app
